eb0abddcc956eed6cfdc4f2fccb0126857d10917
AXG25 <axiever.guejia@gmail.com>
2021-08-18 12:26:09 -0500

    creando clase planeta

---
+++ scripts/planetas.js
@@ -0,0 +1,10 @@
+class Planeta{
+    constructor(nombre_planeta){
+        this.numero_clasificacion = 0;
+        this.nombre = nombre_planeta;
+        this.distancia = 0;
+        this.tamaño = 0;
+    }
+}
+
+export default Planeta;
